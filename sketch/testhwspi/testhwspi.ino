b24a6cfffc974bd1a239128cfc7c68c78d30a3d0
Alvaro <alvieboy@alvie.com>
2012-05-28 19:17:39 +0100

    fix build

--- sketch/testhwspi/testhwspi.ino
+++ sketch/testhwspi/testhwspi.ino
@@ -7,12 +7,13 @@ unsigned int outputarray[512];
 void setup()
 {
 	REGISTER(HWMULTISPIBASE,1)=0; // SPI flash offset
-	REGISTER(HWMULTISPIBASE,2)= &outputarray[0]; // base memory address
+	REGISTER(HWMULTISPIBASE,2)= (unsigned)&outputarray[0]; // base memory address
 	REGISTER(HWMULTISPIBASE,3)= 1577; // One more than number of leds
 
 	// Fill in the output array
 
 	outputarray[0] = 0; // First value is zero, so we can flush the strips easily
+	int i;
 
 	for (i=1;i<512;i++) {
 		outputarray[i] = 0xff00ff;
