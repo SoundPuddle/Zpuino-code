7270b942572dea9ff490caf3b1bfdd720ce1c163
Alvaro <alvieboy@alvie.com>
2012-05-30 16:51:56 +0100

    New tester

--- utils/create-map-from-csv.pl
+++ utils/create-map-from-csv.pl
@@ -29,12 +29,17 @@ for (my $i=0;$i<8;$i++) {
 print STDERR "\nTotal leds: $total\n";
 print STDERR "FFT range $min to $max\n";
 
-print STDERR "Dumping to binary file 'out.bin'";
+print STDERR "Dumping to binary file 'out.bin'\n";
 
 # First, add flush code
+# ((n + 63) / 64) * 3
 
 for (my $i=0;$i<8;$i++) {
     my $cnt = $sizes[$i];
+    
+    $cnt+=63;
+    $cnt = int(($cnt/64));
+    
     while ($cnt) {
         push(@{$controllers[$i]},0);
         $cnt--;
@@ -56,4 +61,48 @@ for(;;) {
     }
     last unless $found;
 };
+
+print STDERR "Writing direct mapping at ", tell $out, "\n";
+
+for (my $i=0;$i<8;$i++) {
+    my $items=0;
+    my $cnt = $sizes[$i];
+    my $off=4;
+    while ($cnt) {
+        print $out pack("Cn",$i,$off);
+        $off+=4;
+        $cnt--;
+        $items++;
+    }
+    # Write flush code
+    
+    $cnt = $sizes[$i];
+
+    $cnt+=63;
+    $cnt = int(($cnt/64));
+
+    while ($cnt) {
+        print $out pack("Cn",$i,0);
+        $items++;
+        $cnt--;
+    }
+    print STDERR "End controller $i at ",tell($out)," ($items items)\n";
+}
+
+print STDERR "Writing flush code at ", tell $out, "\n";
+
+for (my $i=0;$i<8;$i++) {
+    my $cnt = $sizes[$i];
+
+    $cnt+=63;
+    $cnt = int(($cnt/64));
+
+    while ($cnt) {
+        print $out pack("Cn",$i,0);
+        $cnt--;
+    }
+}
+
+print STDERR "End at ", tell $out, "\n";
+
 close $out;
