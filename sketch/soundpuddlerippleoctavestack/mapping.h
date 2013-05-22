a87b0f07f17fe74537c112c6e3494051583d70c9
John English <john@x120e-john.(none)>
2013-05-22 02:24:41 -0600

    creation of a utility python script for calculating optimal sample rate and BIN; creation of soundpuddlerippleoctavestack.ino, which attemps to modify the buffers to allow each LED strip to represent the average of a set of BIN

---
+++ sketch/soundpuddlerippleoctavestack/mapping.h
@@ -0,0 +1,8 @@
+#ifndef __MAPPING_H__
+#define __MAPPING_H__
+extern unsigned int ledmapping[]; 
+#define FLUSH_OFFSET 3912 /* In words */
+#define FLUSH_SIZE 36 /* In words */
+#define DIRECTMAP_OFFSET 1956 /* In words */ 
+#define NUMLEDS 1920 
+#endif
