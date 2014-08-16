fc6e115709fda335e508bcae6d7c3d005045b94f
John English <john@boulderes.com>
2014-08-15 23:26:47 -0600

    soundpuddle 36 spoke progress, at BES prior to BRC2014

--- sketch/soundpuddleripple/mapping.h
+++ sketch/soundpuddleripple/mapping.h
@@ -1,8 +1,8 @@
 #ifndef __MAPPING_H__
 #define __MAPPING_H__
 extern unsigned int ledmapping[]; 
-#define FLUSH_OFFSET 7800 /* In words */
-#define FLUSH_SIZE 60 /* In words */
-#define DIRECTMAP_OFFSET 3900 /* In words */ 
-#define NUMLEDS 3840 
+#define FLUSH_OFFSET 9360 /* In words */
+#define FLUSH_SIZE 72 /* In words */
+#define DIRECTMAP_OFFSET 4680 /* In words */ 
+#define NUMLEDS 4608 
 #endif
