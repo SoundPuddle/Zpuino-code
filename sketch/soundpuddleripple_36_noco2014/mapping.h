6aeb669cd488f9506a7d097d07488008ecc2b533
Jone Lay <jlay.dev@pm.me>
2014-10-05 11:54:35 -0600

    adding the soundpuddle zpuino files from Saturday at the 2014 NoCo Maker Faire. This matches the "So What" Miles Davis video that was put up on youtube

---
+++ sketch/soundpuddleripple_36_noco2014/mapping.h
@@ -0,0 +1,8 @@
+#ifndef __MAPPING_H__
+#define __MAPPING_H__
+extern unsigned int ledmapping[]; 
+#define FLUSH_OFFSET 9360 /* In words */
+#define FLUSH_SIZE 72 /* In words */
+#define DIRECTMAP_OFFSET 4680 /* In words */ 
+#define NUMLEDS 4608 
+#endif
