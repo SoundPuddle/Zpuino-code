ee7d50088674775f548b86c50b9075329ccc1517
John English <john@boulderes.com>
2014-12-16 20:58:36 -0700

    Various Zpuino 'sketches' from 2014-05 - 2014-10 for trying out new features

---
+++ sketch/soundpuddleripple36/mapping.h
@@ -0,0 +1,8 @@
+#ifndef __MAPPING_H__
+#define __MAPPING_H__
+extern unsigned int ledmapping[]; 
+#define FLUSH_OFFSET 7800 /* In words */
+#define FLUSH_SIZE 60 /* In words */
+#define DIRECTMAP_OFFSET 3900 /* In words */ 
+#define NUMLEDS 3840 
+#endif
