ad757499213e494b17f9f65f7570c7be205d7503
Jone Lay <jlay.dev@pm.me>
2016-04-18 02:21:29 -0600

    fixed mapping bug for new backpack soundpuddle, working basic ripple mode demo

--- sketch/soundpuddle_new/soundpuddle.h
+++ sketch/soundpuddle_new/soundpuddle.h
@@ -11,7 +11,7 @@
 #define DEFAULT_ADC_CHANNEL 0x02 // channel 0x02 == on-board electret mic
 
 // LED array dimensions
-#define NUMSPOKES 12 // number of strips (index at 1, i.e. 12 strips = 12)
+#define NUMSPOKES 24 // number of strips (index at 1, i.e. 12 strips = 12)
 #define SPOKEBUFFERSIZE 16 // number of leds per strip + start and stop frame count (ex: 16 APA102 LEDs with 1 START frame and 1 STOP frame would have SPOKEBUFFERSIZE == 18)
 
 // LED packets (vary by LED type)
