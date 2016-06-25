7504defb07e2da8ba39cc55661fad2dcb442a505
John English <john@boulderes.com>
2016-06-24 21:31:44 -0600

    test candidate 16x58 LED program for Chris Chronopolus. The sketch runs a 24x58 array, to index all 12 drives. Lightly tested with a single strip

---
+++ mappings/led/chrono/create-map-from-csv.pl
@@ -0,0 +1,139 @@
+#!/usr/bin/perl
+use strict;
+my @controllers;
+my @sizes;
+
+my $max=0;
+my $min=9999999;
+
+my $numctrl;
+
+while (<STDIN>) {
+    chomp;
+    my @values = split(/[,\s]/);
+    
+    $numctrl||=scalar(@values);
+    
+    for (my $i=0;$i<$numctrl;$i++) {
+        my $val = shift @values;
+        next unless $val=~/[0-9]+/;
+        push(@{$controllers[$i]},$val+1); # Need to add offset
+        $min=$val if $val<$min;
+        $max=$val if $val>$max;
+    }
+}
+print STDERR "Controller (we have $numctrl) dump: \n";
+my $total=0;
+for (my $i=0;$i<$numctrl;$i++) {
+    my $cnt = scalar(@{$controllers[$i]});
+    print STDERR $i, ": ", $cnt, " leds\n";
+    @sizes[$i] = $cnt;
+    $total+=$cnt;
+}
+
+print STDERR "\nTotal leds: $total\n";
+print STDERR "FFT range $min to $max\n";
+
+print STDERR "Dumping to binary file 'out.bin'\n";
+
+# First, add flush code
+# ((n + 63) / 64) * 3
+
+for (my $i=0;$i<$numctrl;$i++) {
+    my $cnt = $sizes[$i];
+    
+    $cnt+=63;
+    $cnt = int(($cnt/64));
+    #print STDERR "Flush for channel $i: $cnt\n";
+    while ($cnt) {
+        push(@{$controllers[$i]},0);
+        $cnt--;
+    }
+}
+
+
+open(my $out, '>', "out.bin");
+open( my $outc, '>', "mapping.cpp" );
+
+print $outc "unsigned int ledmapping[] = { \n";
+
+for(;;) {
+    my $found=undef;
+    for (my $i=0;$i<$numctrl;$i++) {
+        my $v = shift(@{$controllers[$i]});
+        if (defined $v) {
+            $found=1;
+            my $offset = $v*4;
+            print $out pack("Cn",$i,$offset);
+            printf $outc "\t0x%08x,\n", $offset + ($i<<16);
+            #printf "CH $i 0x%08x\n", $offset;
+        }
+    }
+    last unless $found;
+};
+
+print STDERR "Writing direct mapping at ", tell $out, "\n";
+my $offdirectmap = tell($out)/3;
+
+for (my $i=0;$i<$numctrl;$i++) {
+    my $items=0;
+    my $cnt = $sizes[$i];
+    my $off=4;
+    while ($cnt) {
+            print $out pack("Cn",$i,$off);
+            printf $outc "\t0x%08x,\n", $off + ($i<<16);
+            
+        $off+=4;
+        $cnt--;
+        $items++;
+    }
+    # Write flush code
+    
+    $cnt = $sizes[$i];
+
+    $cnt+=63;
+    $cnt = int(($cnt/64));
+
+    while ($cnt) {
+            print $out pack("Cn",$i,0);
+            printf $outc "\t0x%08x,\n", ($i<<16);
+            
+        $items++;
+        $cnt--;
+    }
+    print STDERR "End controller $i at ",tell($out)," ($items items)\n";
+}
+
+print STDERR "Writing flush code at ", tell $out, "\n";
+my $flushsize=0;
+my $flushoff_words= tell($out)/3;
+
+for (my $i=0;$i<$numctrl;$i++) {
+    my $cnt = $sizes[$i];
+
+    $cnt+=63;
+    $cnt = int(($cnt/64));
+
+    while ($cnt) {
+        print $out pack("Cn",$i,0);
+        my $sep = ($cnt==1 && $i==($numctrl-1)) ? "":",";
+        printf $outc "\t0x%08x".$sep."\n", 0 + ($i<<16);
+        $flushsize++;
+        $cnt--;
+    }
+}
+print $outc "};\n";
+
+print STDERR "End at ", tell $out, "\n";
+
+close $outc;
+open($outc, '>', "mapping.h");
+print $outc "#ifndef __MAPPING_H__\n";
+print $outc "#define __MAPPING_H__\n";
+print $outc "extern unsigned int ledmapping[]; \n";
+print $outc "#define FLUSH_OFFSET $flushoff_words /* In words */\n";
+print $outc "#define FLUSH_SIZE $flushsize /* In words */\n";
+print $outc "#define DIRECTMAP_OFFSET $offdirectmap /* In words */ \n" ;
+print $outc "#define NUMLEDS $total \n" ;
+print $outc "#endif\n" ;
+close $out;
---
+++ mappings/led/chrono/mapping.h
@@ -0,0 +1,8 @@
+#ifndef __MAPPING_H__
+#define __MAPPING_H__
+extern unsigned int ledmapping[]; 
+#define FLUSH_OFFSET 2832 /* In words */
+#define FLUSH_SIZE 24 /* In words */
+#define DIRECTMAP_OFFSET 1416 /* In words */ 
+#define NUMLEDS 1392 
+#endif
